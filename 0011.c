7796d5f81e49e30660057dbc64026db82c2bdf7b
Jones. <jonesmailhub@gmail.com>
2026-03-06 18:17:42 +0500

    Create 0011.c

---
+++ 0011.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    short int num = 32767;
+    int num2 = 2000000;
+    
+    unsigned short int n = 12345; // 0-65000
+    signed short int v = -32768; // -32768 - 32767
+    unsigned int a = 45774; // 0-4 млн
+    signed int b = -56888; // -2 - 2 млн
+    unsigned int y = 8;
+    unsigned long long int n umberhumber = 467764357;
+    
+    printf("Hello world!");
+    return 0;
+}
