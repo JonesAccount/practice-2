a83ed25fab23adab0b8b4677272ffc8ba765ad5f
Jones. <jonesmailhub@gmail.com>
2026-03-06 18:15:29 +0500

    Create 0005.c

---
+++ 0005.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    unsigned char var1;
+    int var2;
+    float var3;
+    short int var4;
+    signed long int var5;
+    long long int var6;
+    double var7;
+    long double var8;
+    
+    printf("Hi");
+    return 0;
+}
