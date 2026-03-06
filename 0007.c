21c044fc1340d4dfdb67aa6dd533d837cb1f8a55
Jones. <jonesmailhub@gmail.com>
2026-03-06 18:16:21 +0500

    Create 0007.c

---
+++ 0007.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    
+    int num = 500;
+    float num2 = 6.6;
+    
+    num = 100;
+    
+    int a, b, c;
+    int q = 111, w, e = 10;
+    w = 99;
+    
+    
+    
+    int number1, number2;
+    number1 = 8;
+    number2 = 5;
+    
+    int res1, res2;
+    
+    res1 = number1 + number2;
+    res2 = number1 - number2;
+    int res3 = number1 * number2;
+    int res4 = number1 / number2;
+    
+    
+    int nums = 1;
+    nums += 5;
+    nums -= 3;
+    
+    nums++; // добавляю единицу
+    
+    
+    printf("Привет!\n");
+    printf("Результат: %d", res3);
+    system("pause");
+    
+    return 0;
+}
