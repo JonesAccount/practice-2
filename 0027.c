d46f7783da10208541c228a0f276e62902f7f638
jons <nooneprofessor@gmail.com>
2026-03-08 12:23:38 +0500

    commit

---
+++ 0027.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int up(void) {
+
+  number++;
+  main()
+
+      return 0;
+}
+
+int main(void) {
+
+  short int number = 0, choice;
+
+  while (1) {
+    printf("Число: %hi\n", number);
+    printf("[1] Увеличить\n[2] Уменьшить\n[3] По умолчание\n");
+    printf(">>> ");
+    scanf("%hi", &choice);
+
+    switch (choice) {
+    case 1:
+      system("clear");
+    }
+  }
+
+  return 0;
+}
