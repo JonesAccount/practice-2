6b3bffe67a4e125a99b18998bdd49735b520555d
Jones. <jonesmailhub@gmail.com>
2026-03-10 15:39:21 +0500

    Create 0076.c

---
+++ 0076.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+char name() {
+    printf("Название книги\n");
+    printf("$ ");
+    //scanf("%s", name)
+    
+    
+}
+
+char add() {
+    printf("ПОЛКА 1\n\n");
+    getchar(); getchar();
+}
