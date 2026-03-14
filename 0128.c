c0d672cee76a028193be546fc22e8b09ffb98596
Jones. <jonesmailhub@gmail.com>
2026-03-14 13:41:50 +0500

    Create 0128.c

---
+++ 0128.c
@@ -0,0 +1,16 @@
+char * pD = &dot;
+    bool separator = false;
+    
+    srand(time(NULL));
+    
+    while (1) {
+        
+        
+        //for (short i = 0; i < 15; i++)
+            //printf("%s ", pD);
+            //separator = rand() % 2;
+        
+        getchar();
+        getchar();
+    }
+}
