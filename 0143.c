9c65a6dd0657b1877dcca2779de5d6dff67c2be3
Jones. <jonesmailhub@gmail.com>
2026-03-17 12:53:22 +0500

    Create 0143.c

---
+++ 0143.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+#define A return
+#define D int
+#define M main
+#define V void
+#define P printf
+#define S scanf
+#define O for
+#define BN {
+#define B }
+#define FA (
+#define F )
+#define G ;
+#define Y 0
+#define Z \n
+#define W <
+#define R ,
+#define HQ &
+#define EZ -
+#define X =
+#define XC +=
+#define JK %
+
+
+D M FA V F BN
+    D SQ R FQ R CQ G P FA"U "F G
+    S FA"JK d JK d JK d" R HQ SQ R HQ FQ R HQ CQ F G
+    O FA D I X SQ G I W FQ G I XC CQ F BN
+        P FA"EZ JK d Z " R I F G
+    B
+    A Y G
+B
