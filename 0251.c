8208209452209675ee120b2befc95fdf6b122cea
Jones. <jonesmailhub@gmail.com>
2026-04-05 19:31:06 +0500

    Create 0251.c

---
+++ 0251.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    long double x = 123467745.67864467789;
+
+    printf("%f\n", x);
+    
+    printf("%e\n", x);  
+    printf("%E\n", x);  
+
+    return 0;
+}
