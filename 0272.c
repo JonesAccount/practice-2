8176cc79d9725278a3d4f916effbd9d48c468d30
Jones. <jonesmailhub@gmail.com>
2026-04-08 13:56:01 +0500

    Create 0272.c

---
+++ 0272.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+    int size, counter, map;
+    size = counter = map = 10;
+    
+    printf("size: %d\n", size);
+    printf("counter: %d\n", size);
+    printf("map: %d", size);
+    
+    return 0;
+}
