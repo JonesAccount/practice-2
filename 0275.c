a9800619703fc2236ee691d4ac49681a4093590d
Jones. <jonesmailhub@gmail.com>
2026-04-08 13:56:44 +0500

    Create 0275.c

---
+++ 0275.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void) {
+    int n = 5;
+    printf("%d\n", n);
+    
+    printf("%.2f\n", (float)n);
+    
+    printf("%f\n", (double)n);
+    
+    printf("%d\n", (int)n);
+    
+    printf("%c\n", n * 15);
+    
+    return 0;
+}
