bbbba1c628b280c36617977a666080f4ee775fa7
Jones. <jonesmailhub@gmail.com>
2026-04-13 20:45:16 +0500

    Create 0371.c

---
+++ 0371.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("\033[?25l");
+    
+    int arr[3] = {1, 2, 3};
+    int *p;
+    p = &arr;
+    printf("%p | %d\n", (void *)p, *(int *)(p));
+    printf("%p | %d\n", (void *)(p + 1), *(int *)(p + 1));
+    printf("%p | %d\n", (void *)(p + 2), *(int *)(p + 2));
+    
+    return 0;
+}
+
