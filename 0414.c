557d0603876184f6e2cb10b6420d0e5fb16af389
Jones. <jonesmailhub@gmail.com>
2026-04-17 07:46:18 +0500

    Update 0414.c

--- 0414.c
+++ 0414.c
@@ -6,52 +6,53 @@ void clear_screen(void) {
     fflush(stdout);
 }
 
-void foo(int *arr, int x) {
-	static int index = 0;
+void foo(int **arr, int x) {
+        static int index = 0;
 
-	int *tmp = realloc(arr, (index + 1) * sizeof(int));
-	if (NULL == tmp) { return; }
+        int *tmp = realloc(*arr, (index + 1) * sizeof(int));
+        if (NULL == tmp) { return; }
 
-	arr = tmp;
-	*(arr + index) = x;
+        *arr = tmp;
+        *(*arr + index) = x;
 
-	clear_screen();
+        clear_screen();
 
-	for (int i = 0; i < (index + 1); i++) {
-		printf("%d ", *(arr + i));
-	}
+        for (int i = 0; i < (index + 1); i++) {
+            printf("%d ", *(*arr + i));
+        }
 
-	printf("\n\n");
+        printf("\n\n");
+        index++;
 }
 
 int main(void) {
-	// локальный блок 1
-	{
-		printf("\033[?25l");
-	}
+        // локальный блок 1
+        {
+                printf("\033[?25l");
+        }
 
-	// локальный блок 2
-	{
-		int *arr = (int *)malloc(0);
-		int number = 0;
-		void (*p)(int *, int);
-		p = foo;
-		for (int i = 1;;i++) {
-			printf("%d | число: ", i);
-			scanf("%d", &number);
+        // локальный блок 2
+        {
+                int *arr = NULL;
+                int number = 0;
+                void (*p)(int **, int);
+                p = foo;
+                for (int i = 1;;i++) {
+                        printf("%d | число: ", i);
+                        scanf("%d", &number);
 
-			if (number == 0) { break; }
+                        if (number == 0) { break; }
 
-			p(arr, number);
-		}
-		
-		free(arr);
-	}
+                        p(&arr, number);
+                }
 
-	// локальный блок 3
-	{
-		printf("Завершение...");
-		printf("\033[?25h");
-	}
-	return 0;
+                free(arr);
+        }
+
+        // локальный блок 3
+        {
+                printf("Завершение...");
+                printf("\033[?25h");
+        }
+        return 0;
 }
