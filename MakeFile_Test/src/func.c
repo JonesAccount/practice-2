7ae614312109e283cb0722d02788259e0eaf5c71
jons <nooneprofessor@gmail.com>
2026-04-28 09:08:19 +0500

    commit

---
+++ MakeFile_Test/src/func.c
@@ -0,0 +1,3 @@
+#define SQUARE(x) ((x) * (x))
+
+int square(int y) { return SQUARE(y); }
---
+++ MakeFile_Test/src/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+#include "func.h"
+
+int main(void) {
+	printf("%d\n", square(2 + 3));
+	return 0;
+}
