71efd9f03a7b73635961bcedbb5333baf72a0c13
Jones. <jonesmailhub@gmail.com>
2026-04-30 09:48:43 +0500

    Create 0460.c

---
+++ 0460.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#define Car struct {int speed; char name[]; }
+
+int main(void) {
+    Car car[3];
+    
+    car[0].speed = 100;
+    strcpy(car[0].name, "bmw");
+    
+    printf("Speed: %d\nBrend: %s", car[0].speed, car[0].name);
+    
+    return 0;
+}
