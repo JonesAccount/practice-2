7861e7602a7fa90949c5e9f5211f454b59f6b09d
jons <nooneprofessor@gmail.com>
2026-05-12 16:43:53 +0500

    com

---
+++ 0490.c
@@ -0,0 +1,19 @@
+#include <SDL2/SDL.h>
+
+int main(void) {
+	SDL_Init(SDL_INIT_VIDEO);
+
+	SDL_Window *window = SDL_CreateWindow(
+		"Hello",
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		800, 600,
+		0
+	);
+
+	SDL_Delay(3000);
+
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+	return 0;
+}
