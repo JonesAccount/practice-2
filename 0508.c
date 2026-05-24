28b09fe472cd808c99e842db2733b7e1098f6f1c
jons <nooneprofessor@gmail.com>
2026-05-24 15:05:30 +0500

    c

--- 0508.c
+++ 0508.c
@@ -23,7 +23,14 @@ int main(int argc, char *argv[]) {
 	Move move = {WINDOW_X / 2, WINDOW_Y / 2};
 	
 	Rectangle player = {move.x, move.y - 10, 50, 50};
-	Rectangle wall = {(WINDOW_X - 50) / 2, (WINDOW_Y + 100) / 2, 50, 200};
+	Rectangle star = {move.x, move.y - 10, 50, 50};
+	Rectangle wall1 = {(WINDOW_X - 50)  / 2, (WINDOW_Y + 100) / 2, 50, 200};
+	Rectangle wall2 = {(WINDOW_X - 500) / 2, (WINDOW_Y - 250) / 2, 150, 50};
+	Rectangle wall3 = {(WINDOW_X + 200) / 2, (WINDOW_Y - 250) / 2, 150, 50};
+	Rectangle wall4 = {100, (WINDOW_Y + 500) / 2, WINDOW_X * 0.8, 50};
+	Rectangle wall5 = {100, 100, WINDOW_X * 0.8, 50};
+	Rectangle wall6 = {100, 100, 50, WINDOW_Y * 0.7};
+	Rectangle wall7 = {WINDOW_X * 0.8 + 50, 100, 50, WINDOW_Y * 0.7};
 	
 	while (!WindowShouldClose()) {
 		float oldX = move.x;
@@ -34,8 +41,17 @@ int main(int argc, char *argv[]) {
 		if (IsKeyDown(KEY_LEFT)  || IsKeyDown(KEY_A)) { move.x--; }
 		if (IsKeyDown(KEY_DOWN)  || IsKeyDown(KEY_S)) { move.y++; }
 		if (IsKeyDown(KEY_UP)    || IsKeyDown(KEY_W)) { move.y--; }
+		
+		player.x = move.x;
+		player.y = move.y;
 	
-		if (CheckCollisionRecs(player, wall)) {
+		if (CheckCollisionRecs(player, wall1) ||
+			CheckCollisionRecs(player, wall2) ||
+			CheckCollisionRecs(player, wall3) ||
+			CheckCollisionRecs(player, wall4) ||
+			CheckCollisionRecs(player, wall5) ||
+			CheckCollisionRecs(player, wall6) ||
+			CheckCollisionRecs(player, wall7)) {
 			move.x = oldX;
 			move.y = oldY;
 		}
@@ -48,13 +64,13 @@ int main(int argc, char *argv[]) {
 		DrawRectangleRec(player, (Color){80, 0, 0, 255});
 		
 		// walls
-		Rectangle rect = {100, 100, WINDOW_X * 0.8, WINDOW_Y * 0.7};
-		DrawRectangleLinesEx(rect, 50, BLACK);
-		
-		// walls inside
-		DrawRectangleRec(wall, BLACK);
-		DrawRectangle((WINDOW_X - 500) / 2, (WINDOW_Y - 250) / 2, 150, 50, BLACK);
-		DrawRectangle((WINDOW_X + 200) / 2, (WINDOW_Y - 250) / 2, 150, 50, BLACK);
+		DrawRectangleRec(wall1, BLACK);
+		DrawRectangleRec(wall2, BLACK);
+		DrawRectangleRec(wall3, BLACK);
+		DrawRectangleRec(wall4, BLACK);
+		DrawRectangleRec(wall5, BLACK);
+		DrawRectangleRec(wall6, BLACK);
+		DrawRectangleRec(wall7, BLACK);
 		
 		// score
 		DrawText(TextFormat("Score: %d", 0), 1, 1, 50, BLACK);
