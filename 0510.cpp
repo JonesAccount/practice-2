a5033402415236ff487e1b08f406ad0494056256
jons <nooneprofessor@gmail.com>
2026-05-28 12:12:14 +0500

    com

---
+++ 0510.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, C++!" << std::endl;
+    return 0;
+}
+
+// g++ 0510.cpp -o app && ./app
---
+++ 0511.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+    std::cout << "Second code" << std::endl;
+    std::cout << "I am a programmer" << std::endl;
+    
+    return 0;
+}
