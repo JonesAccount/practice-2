66e1bdca08c15bbf2f59f3f22ff67263a2ade7bb
Jones. <jonesmailhub@gmail.com>
2026-05-29 21:37:49 +0500

    Create 0519.c

---
+++ 0519.c
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    int age = 1;
+    std::cout << "I wanna buy\n" << age << " headphone\n";
+    std::cout << "on Sunday" << std::endl;
+    std::cout << "This is my plan";
+    
+    return 0;
+}
