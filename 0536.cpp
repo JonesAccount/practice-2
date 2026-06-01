e179ec85eca854180e7c144a612b8dd129d57782
Jones. <jonesmailhub@gmail.com>
2026-06-01 17:59:44 +0500

    Create 0536.cpp

---
+++ 0536.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#define NEWLINE "\n"
+#define NOTHING ""
+
+int main() {
+    std::cout << "\033[?25l";
+    
+    for (int i = 1; i < 21; i++) {
+        if (i < 10) { std::cout << "0"; }
+        std::cout << i << " ";
+        (i % 4 == 0) ? std::cout << NEWLINE : std::cout << NOTHING;
+    }
+    
+    return 0;
+}
