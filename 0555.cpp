dcf0fca73a771976bffbbbb10f05727db27d24a5
Jones. <jonesmailhub@gmail.com>
2026-06-06 15:22:27 +0500

    Create 0555.cpp

---
+++ 0555.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    // sigma
+    int million = 1000000;
+    for (int i = 1; i <= million; i++) {
+        std::cout << "Punch Kezia";
+    }
+    return 0;
+}
